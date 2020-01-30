85706871c4a8b2a3de77a795068b01bd2502106c
Alex Higgins <TheAlexanderHiggins@gmail.com>
2020-01-30 13:40:44 -0500

    Changed name of the resource

--- azure-terraform/index.tf
+++ azure-terraform/index.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sg" {
-  name                     = "storageaccountname"
+  name                     = "ds-tmp-storage-account"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -30,7 +30,7 @@ resource "azurerm_storage_account" "sg" {
 
 
 resource "azurerm_storage_container" "container" {
-  name                  = "tmp-container"
+  name                  = "ds-tmp-container"
   storage_account_name  = azurerm_storage_account.sg.name
   container_access_type = "private"
 }
