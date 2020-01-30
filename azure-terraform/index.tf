8e22e380036dc2bb141639903e31187aeb6db312
Alex Higgins <TheAlexanderHiggins@gmail.com>
2020-01-30 11:28:35 -0500

    Added additional azure resources

--- azure-terraform/index.tf
+++ azure-terraform/index.tf
@@ -15,3 +15,23 @@ resource "azurerm_resource_group" "rg" {
   name     = "ds-tmp-resource-group"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "sg" {
+  name                     = "storageaccountname"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  location                 = "${azurerm_resource_group.rg.location}"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+
+resource "azurerm_storage_container" "container" {
+  name                  = "tmp-container"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  storage_account_name  = "${azurerm_storage_account.sg.name}"
+  container_access_type = "private"
+}
