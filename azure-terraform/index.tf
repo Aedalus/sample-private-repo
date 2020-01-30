71ef791e828c0af1d587020d3e457bbb2ff16b45
Alex Higgins <TheAlexanderHiggins@gmail.com>
2020-01-30 13:38:21 -0500

    Fixed storage container properties

--- azure-terraform/index.tf
+++ azure-terraform/index.tf
@@ -18,6 +18,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "sg" {
   name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
@@ -30,7 +31,6 @@ resource "azurerm_storage_account" "sg" {
 
 resource "azurerm_storage_container" "container" {
   name                  = "tmp-container"
-  resource_group_name   = azurerm_resource_group.rg.name
   storage_account_name  = azurerm_storage_account.sg.name
   container_access_type = "private"
 }
